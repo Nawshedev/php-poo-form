acf00ec1c3bd011fc97427437322f5e633495563
Nawel Grine <nawelgrineb@gmail.com>
2022-11-16 09:01:51 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <form action="" method="post">
+
+
+        <label for="firstname">Prénom</label>
+        <input type="text" name="firstname" value="votre prénom">
+
+        <label for="age">Age</label>
+        <input type="text" name="age" value="votre âge">
+    </form>
+
+
+</body>
+
+</html>
